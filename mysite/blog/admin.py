05f8acf747d045ae6622e2174e6c1cf0877228db
Qu0l0 <burchik.99@mail.ru>
2023-07-06 17:43:33 +0600

    settings admin

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from blog.models import Post
+
+
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'published', 'status']
+    list_filter = ['author', 'status', 'created', 'published']
+    search_fields = ['title', 'body', 'author']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'published'
+    ordering = ['status', 'published']
